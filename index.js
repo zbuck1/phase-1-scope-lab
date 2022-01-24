e7fe9f415c467ede88691271eadbacb828ec14fb
Zoe <buckzoe5@gmail.com>
2022-01-24 15:43:11 -0500

    commit tree 32846ff040b1a7a137d678f343f5e73736234434

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "unknown initial value";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+     bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "ozzy";
+}
+
+
